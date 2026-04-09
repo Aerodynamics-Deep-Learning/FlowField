1a42f973f90ba62d83bf35dc4421c76442ab01d4
enatrage <103586619+enatrage@users.noreply.github.com>
2026-04-09 16:07:23 -0400

    feat(datagen): small addition for output conversion
    Added:
    - a function to get the free-stream values, assuming isentropic behavior
    - an unfinished function to convert the dimensionless Cp values to Conservative States

--- src/datagen/solvers/xfoil/utils.py
+++ src/datagen/solvers/xfoil/utils.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 
+import torch
+
 def XFoil_Check_ConvergenceCp(stdout: str, cp_file_path: str, convergence_limit: float=1.0e2) -> int:
     """
     Checks the state of convergence for an XFoil run, given its outputs.
@@ -46,3 +48,52 @@ def XFoil_Check_ConvergenceCp(stdout: str, cp_file_path: str, convergence_limit:
         return 0
         
     return 2
+
+def XFoil_Get_Freestream_Conditions(mach: float, altitude_m: float):
+    """
+    Gets the freestream conditions (P_inf, T_inf, rho_inf), given specific operational condition assumptions using
+    International Standard Atmosphere (ISA) model (altitude_m < 10k) and the mach number.
+
+    Args:
+        mach (float): The mach number used in runs
+        altitufe_m (float): The assumed altitude of the run/setup
+    """
+
+    # ISA constants
+    R = 287.05
+    GAMMA = 1.4
+    T_SL = 288.15
+    P_SL = 101325.0
+    L = 0.0065
+    G = 9.80665
+
+    # Freestream temperature
+    if altitude_m < 11000.0:
+        T_inf = T_SL - L *altitude_m
+    else: 
+        T_inf = 216.5 # Assuming some constant temperature in lower stratosphere
+
+    # Freestream pressure
+    P_inf = P_SL * (T_inf / T_SL)**(G / (R * L))
+    
+    # Freestream density
+    rho_inf = P_inf / (R * T_inf)
+
+    # Freestream velocity magnitude
+    a_inf = np.sqrt(GAMMA * R * T_inf)
+    V_inf = mach * a_inf
+
+    return P_inf, T_inf, rho_inf, V_inf
+
+def XFoil_To_Conservative_State(cp_tensor: torch.Tensor,
+                                coords_tensor: torch.Tensor,
+                                M_inf: float,
+                                P_inf: float,
+                                T_inf: float,
+                                alpha):
+
+    """
+    Conducts the relevant conversion from XFoil variables (dimensionless Cp) into isentropic flow assuming local conservative variables.
+    These variables are used for the warm-start
+    """
+
