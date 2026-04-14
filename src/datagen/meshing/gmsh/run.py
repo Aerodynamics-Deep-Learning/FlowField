f22d875c440aa5fdebd5c0756bd92123f119e706
enatrage <103586619+enatrage@users.noreply.github.com>
2026-04-14 09:40:25 -0400

    feat(datagen): updated mesh setup
    - updated the mesh setup, by using the variable distance-meshsize distances
    - improved fail condition check
    - added a fail condition check for TE blunting

--- src/datagen/meshing/gmsh/run.py
+++ src/datagen/meshing/gmsh/run.py
@@ -1,11 +1,15 @@
 import gmsh
 import os
+import numpy as np
 
 from .io import GMSH_Write_Exception
 from .utils import get_mesh_height
 
 from ...schemas import Airfoil
 from .schemas import GMSH_In, GMSH_Out, GMSH_MeshingConfig, GMSH_ExitFlag
+
+import logging
+logger = logging.getLogger(__name__)
 
 def GMSH_MeshGenerator(data: GMSH_In) -> GMSH_Out:
 
@@ -60,6 +64,28 @@ def GMSH_MeshGenerator(data: GMSH_In) -> GMSH_Out:
             target_yplus= data.meshing_config.target_yplus
         )
 
+        # Checks the trailing edge length, if it is too small or blunted at all
+        te_gap = np.linalg.norm(data.airfoil.coords_tensor[0] - data.airfoil.coords_tensor[-1])
+        if te_gap == 0:
+            logger.warning(f"Airfoil appears to not have been blunted. The first and last points are coincident at {data.airfoil.coords_tensor[0]}, this may cause problems with meshing")
+            return GMSH_Out(
+                airfoil=data.airfoil,
+                freestream=data.freestream,
+                flag=GMSH_ExitFlag.BLUNTING_FAIL,
+                verbose_list=[log_path, None, None]
+            )
+        elif te_gap < 1e-5:
+            logger.warning(f"Trailing edge gap is small: {te_gap}, this will most likely cause problems with TE meshing")
+            return GMSH_Out(
+                airfoil=data.airfoil,
+                freestream=data.freestream,
+                flag=GMSH_ExitFlag.BLUNTING_FAIL,
+                verbose_list=[log_path, None, None]
+            )
+        else:
+            pass
+
+        # Get the mesh
         geo_path = generate_mesh(
             meshing_config= data.meshing_config,
             airfoil= data.airfoil,
@@ -68,20 +94,28 @@ def GMSH_MeshGenerator(data: GMSH_In) -> GMSH_Out:
         )
 
         # Flagging and metrics
-        _, qualities = gmsh.model.mesh.getElementQuality(elementTags=[], qualityName="SICN")
-        min_mesh_quality = min(qualities) if len(qualities) > 0 else 0.0
-        num_nodes = len(gmsh.model.mesh.getNodes()[0])
-
+        min_mesh_quality = 0.0
+        # Extract tags for 2D traingles and quads
+        tri_tags, _ = gmsh.model.mesh.getElementsByType(2)
         quad_tags, _ = gmsh.model.mesh.getElementsByType(3)
+        all_2d_tags = np.concatenate([tri_tags, quad_tags]).astype(np.uint64)
+        if len(all_2d_tags) > 0:
+            _, qualities = gmsh.model.mesh.getElementQuality(elementTags=all_2d_tags, qualityName="SICN")
+            min_mesh_quality = min(qualities)
+        else:
+            flag = GMSH_ExitFlag.EXTRUSION_FAIL
+
+        num_nodes = len(gmsh.model.mesh.getNodes()[0])
         num_quads = len(quad_tags)
 
-        if num_quads == 0:
+        if (num_quads == 0):
             flag = GMSH_ExitFlag.EXTRUSION_FAIL
         elif min_mesh_quality <= 0.1:
             flag = GMSH_ExitFlag.NEGATIVE_JACOBIAN
         else:
             flag = GMSH_ExitFlag.SUCCESS
 
+        gmsh.option.setNumber("Mesh.SaveAll", 0)
         mesh_name = f"{data.airfoil.airfoil_name}_mesh.su2"
         mesh_path = os.path.join(data.working_dir, mesh_name)
         gmsh.write(mesh_path)
@@ -166,26 +200,55 @@ def generate_mesh(meshing_config: GMSH_MeshingConfig, airfoil: Airfoil, h_first:
     all_curve_tags = [tag for dim, tag in all_domain_boundaries]
     actual_airfoil_curves = []
     actual_farfield_curves = []
-
+    # Manually picking out the actual airfoil and farfield curves bcs of cut oper
     for tag in all_curve_tags:
         bbox = gmsh.model.occ.getBoundingBox(1, tag)
         if abs(bbox[0]) > 5.0 or abs(bbox[3]) > 5.0: 
             actual_farfield_curves.append(tag)
         else:
             actual_airfoil_curves.append(tag)
-
     fluid_surface_tags = [tag for dim, tag in fluid_domain]
-
     
-    # Defining boundary layer field
+    # FIELDS:
+    # Defining boundary layer field and meshing configs
     field_id = model.mesh.field.add("BoundaryLayer")
     model.mesh.field.setNumbers(field_id, "CurvesList", actual_airfoil_curves)
+
     model.mesh.field.setNumber(field_id, "Size", h_first)
     model.mesh.field.setNumber(field_id, "Ratio", meshing_config.bl_growth_ratio)
     model.mesh.field.setNumber(field_id, "Quads", 1)
     model.mesh.field.setNumber(field_id, "Thickness", meshing_config.bl_total_thickness)
     model.mesh.field.setNumber(field_id, "FanNodes", meshing_config.bl_fan_elements)
     model.mesh.field.setAsBoundaryLayer(field_id)
+
+    # Main airfoil field, defined by the LE distance
+    dist_le = model.mesh.field.add("Distance")
+    model.mesh.field.setNumbers(dist_le, "CurvesList", [upper_spline, lower_spline])
+    model.mesh.field.setNumber(dist_le, "Sampling", 1000)
+
+    thresh_le = model.mesh.field.add("Threshold")
+    model.mesh.field.setNumber(thresh_le, "InField", dist_le)
+    model.mesh.field.setNumber(thresh_le, "SizeMin", meshing_config.lc_leadingedge)
+    model.mesh.field.setNumber(thresh_le, "SizeMax", meshing_config.lc_farfield)
+    model.mesh.field.setNumber(thresh_le, "DistMin", 0.1)
+    model.mesh.field.setNumber(thresh_le, "DistMax", meshing_config.farfield_radius * 0.5)
+
+    # The trailing edge field, defined by distance to the TE line
+    dist_te = model.mesh.field.add("Distance")
+    model.mesh.field.setNumbers(dist_te, "CurvesList", [te_line])
+    model.mesh.field.setNumber(dist_te, "Sampling", 100)
+
+    thresh_te = model.mesh.field.add("Threshold")
+    model.mesh.field.setNumber(thresh_te, "InField", dist_te)
+    model.mesh.field.setNumber(thresh_te, "SizeMin", meshing_config.lc_trailingedge)
+    model.mesh.field.setNumber(thresh_te, "SizeMax", meshing_config.lc_farfield)
+    model.mesh.field.setNumber(thresh_te, "DistMin", 0.05)
+    model.mesh.field.setNumber(thresh_te, "DistMax", meshing_config.farfield_radius * 0.5)
+
+    # Combine all the fields using a min, and define the background mesh
+    min_id = model.mesh.field.add("Min")
+    model.mesh.field.setNumbers(min_id, "FieldsList", [field_id, thresh_le, thresh_te])
+    model.mesh.field.setAsBackgroundMesh(min_id)
 
     # Defining the groups
     gmsh.model.addPhysicalGroup(1, actual_airfoil_curves, name=meshing_config.marker_airfoil)
@@ -200,6 +263,7 @@ def generate_mesh(meshing_config: GMSH_MeshingConfig, airfoil: Airfoil, h_first:
     geo_path = os.path.join(working_dir, f"{airfoil.airfoil_name}.geo")
     gmsh.write(geo_path)
 
+    # Generate the mesh, using the defined configs, groups, and fields
     model.mesh.generate(2)
 
     return geo_path
